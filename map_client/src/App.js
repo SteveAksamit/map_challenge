0c19bb77aff7b3afb4f5e2fc14e83f840d6eb647
Steve Aksamit <steveaksamit@gmail.com>
2021-04-21 17:16:30 -0500

    added select box with ranges

--- map_client/src/App.js
+++ map_client/src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {states: []}
+  state = {
+    states: {},
+    rangeOptions: [
+      { key: 1, visitRange: '0 - 250', statesInRange: [] },
+      { key: 2, visitRange: '250 - 500', statesInRange: [] },
+      { key: 3, visitRange: '500 - 1000', statesInRange: [] },
+      { key: 4, visitRange: '1000+', statesInRange: [] }
+    ]
+  }
 
   componentDidMount() {
     fetch('/states')
       .then(res => res.json())
       .then(states => {
-        const cleanedStates = this.cleanData(states)
+        const cleanedStates = this.cleanData(states);
         this.setState({ states: cleanedStates });
       })
   };
@@ -24,7 +32,6 @@ class App extends Component {
         }
       }
     })
-    console.log(aggregateData)
     return aggregateData;
   }
 
@@ -32,7 +39,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1>States</h1>
-
+        <select>
+            <option key="placeholder">Select an option</option>
+            {this.state.rangeOptions.map(option =>(
+              <option key={option.key}>{option.visitRange}</option>
+            ))}
+        </select>
       </div>
     );
   }
