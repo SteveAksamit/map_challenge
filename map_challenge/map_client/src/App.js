d014dca49ed031b827b7338100f34d5ff5a4e4d4
Steve Aksamit <steveaksamit@gmail.com>
2021-04-21 13:50:52 -0500

    client and server working

---
+++ map_challenge/map_client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {states: []}
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(states => this.setState({ states }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>States</h1>
+        {this.state.states.map(state =>
+          <div key={state.id}>{state.name}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ map_challenge/map_server/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const stateData = require('../data/stateData.json')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.json(stateData);
+});
+
+module.exports = router;
