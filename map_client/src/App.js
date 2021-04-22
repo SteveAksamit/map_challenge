d36672ca83a1dc82dab06cc056a87bd7bf4a25d1
Steve Aksamit <steveaksamit@gmail.com>
2021-04-22 17:22:22 -0500

    select highlighting states properly

--- map_client/src/App.js
+++ map_client/src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, ReactDOM } from 'react';
 import './App.css';
 import { ReactComponent as MapSvg } from './map.svg'
 
 class App extends Component {
-  state = {
-    rangeOptions: [
-      { visitRange: '0 - 250', statesInRange: [] },
-      { visitRange: '250 - 500', statesInRange: [] },
-      { visitRange: '500 - 1000', statesInRange: [] },
-      { visitRange: '1000+', statesInRange: [] }
-    ]
+  constructor(props) {
+    super(props);
+    this.mapRef = React.createRef();
+    this.state = {
+      rangeOptions: [
+        { value: 'range1', visitRange: '0 - 250', statesInRange: [] },
+        { value: 'range2', visitRange: '250 - 500', statesInRange: [] },
+        { value: 'range3', visitRange: '500 - 1000', statesInRange: [] },
+        { value: 'range4', visitRange: '1000+', statesInRange: [] }
+      ],
+      selectedVal: 'placeholder'
+    }
+    this.handleSelect = this.handleSelect.bind(this);
   }
+
+
 
   componentDidMount() {
     fetch('/states')
@@ -43,7 +51,20 @@ class App extends Component {
             break;
         }
       }
-      console.log(this.state.rangeOptions)
+    })
+  }
+
+  handleSelect(event){
+    const selectedOpt = event.target.value;
+    console.log(selectedOpt)
+    this.setState({'selectedVal': selectedOpt});
+    const selectedRange = this.state.rangeOptions.find(range => range.value === selectedOpt)
+    const mapDomElement = this.mapRef.current;
+    selectedRange.statesInRange.map(stateCode => {
+      const statePathDomNode = mapDomElement.getElementsByClassName(stateCode);
+      if(statePathDomNode && statePathDomNode[0] && statePathDomNode[0].setAttribute){
+        statePathDomNode[0].setAttribute('class', 'active')
+      }
     })
   }
 
@@ -51,14 +72,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>States</h1>
-        <select>
-            <option key="placeholder">Select an option</option>
+        <select onChange={this.handleSelect} value={this.state.selectedVal}>
+            <option key="placeholder" value="placeholder">Select an option</option>
             {this.state.rangeOptions.map((option, i) =>(
-              <option key={i + 1}>{option.visitRange}</option>
+              <option key={i + 1} value={option.value}>{option.visitRange}</option>
             ))}
         </select>
         <div>
-          <MapSvg />
+          <MapSvg ref={this.mapRef}/>
         </div>
       </div>
     );
