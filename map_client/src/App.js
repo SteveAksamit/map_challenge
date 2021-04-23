712c4cde66a6ca881273cf5e55dfbb35b3eb3308
Steve Aksamit <steveaksamit@gmail.com>
2021-04-23 15:38:19 -0500

    ran prettier on all files

--- map_client/src/App.js
+++ map_client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component, ReactDOM } from 'react';
-import './App.css';
-import { ReactComponent as MapSvg } from './map.svg'
+import React, { Component, ReactDOM } from "react";
+import "./App.css";
+import { ReactComponent as MapSvg } from "./map.svg";
 
 class App extends Component {
   constructor(props) {
@@ -8,75 +8,79 @@ class App extends Component {
     this.mapRef = React.createRef();
     this.state = {
       rangeOptions: [
-        { value: 'placeholder', visitRange: 'Select an option', statesInRange: [] },
-        { value: 'range1', visitRange: '0 - 250', statesInRange: [] },
-        { value: 'range2', visitRange: '250 - 500', statesInRange: [] },
-        { value: 'range3', visitRange: '500 - 1000', statesInRange: [] },
-        { value: 'range4', visitRange: '1000+', statesInRange: [] }
+        {
+          value: "placeholder",
+          visitRange: "Select an option",
+          statesInRange: [],
+        },
+        { value: "range1", visitRange: "0 - 250", statesInRange: [] },
+        { value: "range2", visitRange: "250 - 500", statesInRange: [] },
+        { value: "range3", visitRange: "500 - 1000", statesInRange: [] },
+        { value: "range4", visitRange: "1000+", statesInRange: [] },
       ],
-      selectedVal: 'placeholder',
-      prevSelectedVal: 'placeholder'
-    }
+      selectedVal: "placeholder",
+      prevSelectedVal: "placeholder",
+    };
     this.handleSelect = this.handleSelect.bind(this);
   }
 
   componentDidMount() {
-    fetch('/states')
-      .then(res => res.json())
-      .then(states => {
+    fetch("/states")
+      .then((res) => res.json())
+      .then((states) => {
         this.mapVisitsToRange(states);
-      })
-  };
+      });
+  }
 
-  mapVisitsToRange(states = []){
+  mapVisitsToRange(states = []) {
     const stateSet = new Set();
-    states.map((state) =>{
+    states.map((state) => {
       const stateCode = state.id.toLowerCase();
-      if(!stateSet.has(stateCode)){
+      if (!stateSet.has(stateCode)) {
         stateSet.add(stateCode);
-        switch(true){
-          case (state.visits <= 250):
+        switch (true) {
+          case state.visits <= 250:
             this.state.rangeOptions[1].statesInRange.push(stateCode);
             break;
-          case (state.visits > 250 && state.visits <= 500):
+          case state.visits > 250 && state.visits <= 500:
             this.state.rangeOptions[2].statesInRange.push(stateCode);
             break;
-          case (state.visits > 500 && state.visits <= 1000):
+          case state.visits > 500 && state.visits <= 1000:
             this.state.rangeOptions[3].statesInRange.push(stateCode);
             break;
-          case (state.visits > 1000):
+          case state.visits > 1000:
             this.state.rangeOptions[4].statesInRange.push(stateCode);
             break;
           default:
             break;
         }
       }
-    })
+    });
   }
 
-  getRange(rangeValue){
-    return this.state.rangeOptions.find(range => range.value === rangeValue)
+  getRange(rangeValue) {
+    return this.state.rangeOptions.find((range) => range.value === rangeValue);
   }
 
-  changeColorOfStates(stateArray, highlightState){
+  changeColorOfStates(stateArray, highlightState) {
     const mapDomElement = this.mapRef.current;
-    stateArray.map(stateCode => {
+    stateArray.map((stateCode) => {
       const stateDomNode = mapDomElement.getElementsByClassName(stateCode);
-      if(stateDomNode[0] && highlightState){
-        stateDomNode[0].classList.add('active');
-      } else if (stateDomNode[0] && !highlightState){
-        stateDomNode[0].classList.remove('active');
+      if (stateDomNode[0] && highlightState) {
+        stateDomNode[0].classList.add("active");
+      } else if (stateDomNode[0] && !highlightState) {
+        stateDomNode[0].classList.remove("active");
       }
-    })
+    });
   }
 
-  handleSelect(event){
+  handleSelect(event) {
     const selectedOpt = event.target.value;
     const prevSelectedOpt = this.state.prevSelectedVal;
-    this.setState({'selectedVal': selectedOpt, 'prevSelectedVal': selectedOpt});
+    this.setState({ selectedVal: selectedOpt, prevSelectedVal: selectedOpt });
     const selectedRange = this.getRange(selectedOpt);
     const prevSelectedRange = this.getRange(prevSelectedOpt);
-    this.changeColorOfStates(prevSelectedRange.statesInRange,false);
+    this.changeColorOfStates(prevSelectedRange.statesInRange, false);
     this.changeColorOfStates(selectedRange.statesInRange, true);
   }
 
@@ -84,13 +88,19 @@ class App extends Component {
     return (
       <div className="App">
         <h4 class="visits-title">User Visits</h4>
-        <select onChange={this.handleSelect} value={this.state.selectedVal} className="visits-select">
-            {this.state.rangeOptions.map((option, i) =>(
-              <option key={i + 1} value={option.value}>{option.visitRange}</option>
-            ))}
+        <select
+          onChange={this.handleSelect}
+          value={this.state.selectedVal}
+          className="visits-select"
+        >
+          {this.state.rangeOptions.map((option, i) => (
+            <option key={i + 1} value={option.value}>
+              {option.visitRange}
+            </option>
+          ))}
         </select>
         <div>
-          <MapSvg ref={this.mapRef} className="svgMap"/>
+          <MapSvg ref={this.mapRef} />
         </div>
       </div>
     );
--- map_server/routes/states.js
+++ map_server/routes/states.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const stateData = require('../data/stateData.json')
+const stateData = require("../data/stateData.json");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   //res.send('respond with a resource');
   res.json(stateData);
 });
