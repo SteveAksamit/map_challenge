d96220bf78c7e39df7591dcf90e3834a506808ec
Steve Aksamit <steveaksamit@gmail.com>
2021-04-23 08:47:36 -0500

    optimized select handler function

--- map_client/src/App.js
+++ map_client/src/App.js
@@ -8,12 +8,14 @@ class App extends Component {
     this.mapRef = React.createRef();
     this.state = {
       rangeOptions: [
+        { value: 'placeholder', visitRange: 'Select an option', statesInRange: [] },
         { value: 'range1', visitRange: '0 - 250', statesInRange: [] },
         { value: 'range2', visitRange: '250 - 500', statesInRange: [] },
         { value: 'range3', visitRange: '500 - 1000', statesInRange: [] },
         { value: 'range4', visitRange: '1000+', statesInRange: [] }
       ],
-      selectedVal: 'placeholder'
+      selectedVal: 'placeholder',
+      prevSelectedVal: 'placeholder'
     }
     this.handleSelect = this.handleSelect.bind(this);
   }
@@ -36,16 +38,16 @@ class App extends Component {
         stateSet.add(stateCode);
         switch(true){
           case (state.visits <= 250):
-            this.state.rangeOptions[0].statesInRange.push(stateCode);
-            break;
-          case (state.visits > 250 && state.visits <= 500):
             this.state.rangeOptions[1].statesInRange.push(stateCode);
             break;
-          case (state.visits > 500 && state.visits <= 1000):
+          case (state.visits > 250 && state.visits <= 500):
             this.state.rangeOptions[2].statesInRange.push(stateCode);
             break;
-          case (state.visits > 1000):
+          case (state.visits > 500 && state.visits <= 1000):
             this.state.rangeOptions[3].statesInRange.push(stateCode);
+            break;
+          case (state.visits > 1000):
+            this.state.rangeOptions[4].statesInRange.push(stateCode);
             break;
           default:
             break;
@@ -54,18 +56,31 @@ class App extends Component {
     })
   }
 
-  handleSelect(event){
-    const selectedOpt = event.target.value;
-    console.log(selectedOpt)
-    this.setState({'selectedVal': selectedOpt});
-    const selectedRange = this.state.rangeOptions.find(range => range.value === selectedOpt)
+  getRange(rangeValue){
+    const foundRange = this.state.rangeOptions.find(range => range.value === rangeValue)
+    return foundRange
+  }
+
+  changeColorOfStates(stateArray, highlightState){
     const mapDomElement = this.mapRef.current;
-    selectedRange.statesInRange.map(stateCode => {
-      const statePathDomNode = mapDomElement.getElementsByClassName(stateCode);
-      if(statePathDomNode && statePathDomNode[0] && statePathDomNode[0].setAttribute){
-        statePathDomNode[0].setAttribute('class', 'active')
+    stateArray.map(stateCode => {
+      const stateDomNode = mapDomElement.getElementsByClassName(stateCode);
+      if(stateDomNode[0] && highlightState){
+        stateDomNode[0].classList.add('active');
+      } else if (stateDomNode[0] && !highlightState){
+        stateDomNode[0].classList.remove('active');
       }
     })
+  }
+
+  handleSelect(event){
+    const selectedOpt = event.target.value;
+    const prevSelectedOpt = this.state.prevSelectedVal;
+    this.setState({'selectedVal': selectedOpt, 'prevSelectedVal': selectedOpt});
+    const selectedRange = this.getRange(selectedOpt);
+    const prevSelectedRange = this.getRange(prevSelectedOpt);
+    this.changeColorOfStates(prevSelectedRange.statesInRange,false);
+    this.changeColorOfStates(selectedRange.statesInRange, true);
   }
 
   render() {
@@ -73,7 +88,6 @@ class App extends Component {
       <div className="App">
         <h1>States</h1>
         <select onChange={this.handleSelect} value={this.state.selectedVal}>
-            <option key="placeholder" value="placeholder">Select an option</option>
             {this.state.rangeOptions.map((option, i) =>(
               <option key={i + 1} value={option.value}>{option.visitRange}</option>
             ))}
